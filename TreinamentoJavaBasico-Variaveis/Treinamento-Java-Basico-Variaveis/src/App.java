e012dade3cc7c3df4162ac9e287deaafcc471d00
Albino Pires <albinofp34@gmail.com>
2024-11-12 20:22:11 -0300

    Iniciando o treinamento com Tipos de Variaveis em JAVA

---
+++ TreinamentoJavaBasico-Variaveis/Treinamento-Java-Basico-Variaveis/src/App.java
@@ -0,0 +1,7 @@
+//Treinamento Java Basico - Tipos de variaveis
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
